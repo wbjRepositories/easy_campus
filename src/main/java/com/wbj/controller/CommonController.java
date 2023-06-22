55ca45a3f74e25e3a2f72b6345b265471a8594f3
wbj <wbj15230723726@163.com>
2023-06-22 16:19:21 +0800

    增加了上传和下载文件的接口，增加了全局异常处理器，把日志信息输出到了server.log文件中

--- src/main/java/com/wbj/controller/CommonController.java
+++ src/main/java/com/wbj/controller/CommonController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
 
 @RestController
